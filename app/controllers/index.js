2c8afaa95ff9869fca8f258fa25e779d183d0120
Devin Weaver <suki@tritarget.org>
2014-01-04 17:04:58 -0500

    Add ability to change image
    Uses the strait image property with a url.

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,22 @@
-function doClick(e) {  
-    alert($.label.text);
+// var FileLoader = require("file_loader");
+
+function urlFromSelection(selection) {
+  return "http://localhost:3000/" + selection.toLowerCase() + ".jpg";
 }
+
+function callbackImageChange(e) {
+  var url = urlFromSelection(e.selectedValue[0]);
+  Ti.API.info("Requested URL: " + url);
+  $.callbackImage.image = url;
+}
+
+function promiseImageChange(e) {
+  var url = urlFromSelection(e.selectedValue[0]);
+  Ti.API.info("Requested URL: " + url);
+  $.promiseImage.image = url;
+}
+
+$.callbackPicker.addEventListener("change", callbackImageChange);
+$.promisePicker.addEventListener("change", promiseImageChange);
 
 $.index.open();
