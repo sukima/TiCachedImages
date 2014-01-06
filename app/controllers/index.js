e3d88a0565644277083bf186dc0ac3d6f6a35c8a
Devin Weaver <suki@tritarget.org>
2014-01-05 20:52:47 -0500

    Add initialization for TaableView

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -31,6 +31,32 @@ function promiseImageChange(e) {
     .done();
 }
 
+(function() {
+  var tags = [
+    "sun",
+    "sea",
+    "tree",
+    "cat",
+    "dog"
+  ];
+
+  function makeRowFor(tag) {
+    var row = Ti.UI.createTableViewRow({ title: tag });
+    FileLoader.download("http://flickholdr.com/128/128/" + tag)
+      .then(function(path) { row.leftImage = path; }).done();
+    return row;
+  }
+
+  var tag_index = 0, i, row, rows = [];
+  for (i = 0; i < 100; i++) {
+    rows.push(makeRowFor(tags[tag_index]));
+    tag_index++;
+    tag_index = (tag_index >= tags.length) ? 0 : tag_index;
+  }
+
+  $.tableList.setData(rows);
+})();
+
 $.callbackPicker.addEventListener("change", callbackImageChange);
 $.promisePicker.addEventListener("change", promiseImageChange);
 
