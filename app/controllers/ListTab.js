8878965729f551105505740a4a16c4683d05573d
Devin Weaver <suki@tritarget.org>
2014-01-17 17:16:03 -0500

    Move asset list to it's own module
    Allows reuse (in server code)

--- app/controllers/ListTab.js
+++ app/controllers/ListTab.js
@@ -1,26 +1,7 @@
 var FileLoader      = require("file_loader");
+var photos          = require("asset_list");
 var use_promises    = true;
 var use_file_loader = true;
-
-var photos = [
-  "IMG_7651_t.png",
-  "IMG_7654_t.png",
-  "IMG_7656_t.png",
-  "IMG_7660_v1_t.png",
-  "IMG_7663_t.png",
-  "IMG_7672_v1_t.png",
-  "IMG_7688_t.png",
-  "IMG_7690_t.png",
-  "IMG_7691_t.png",
-  "IMG_7699_v1_t.png",
-  "IMG_7766_t.png",
-  "IMG_7778_t.png",
-  "IMG_7784_t.png",
-  "IMG_7786_t.png",
-  "IMG_7789_t.png",
-  "IMG_7790_t.png",
-  "IMG_7809_v1_t.png"
-];
 
 function refreshTableData() {
   function makeRowFor(photo) {
---
+++ app/lib/asset_list.js
@@ -0,0 +1,19 @@
+module.exports = [
+  "IMG_7651_t.png",
+  "IMG_7654_t.png",
+  "IMG_7656_t.png",
+  "IMG_7660_v1_t.png",
+  "IMG_7663_t.png",
+  "IMG_7672_v1_t.png",
+  "IMG_7688_t.png",
+  "IMG_7690_t.png",
+  "IMG_7691_t.png",
+  "IMG_7699_v1_t.png",
+  "IMG_7766_t.png",
+  "IMG_7778_t.png",
+  "IMG_7784_t.png",
+  "IMG_7786_t.png",
+  "IMG_7789_t.png",
+  "IMG_7790_t.png",
+  "IMG_7809_v1_t.png"
+];
